ae64160aeaa4b2511b03fef2e88e597154149a96
Cristian Mayo <cristian.c.mayo@outlook.com>
2021-02-04 11:06:58 +0800

    Core Solution Structure (#1)
    * Added Initial Web API Project Solution
    
    * Added _config and docs solution folder
    
    * Added Swagger API Documentation
    
    - Configured integration and updated Open API Info in Startup file
    - Added XML documentation feature in API documentation
    - Set the swagger index as default start page
    
    * Added Nswag Client Generation
    
    - Added new class library project
    - Added nswag file with default configuration to use Swagger Specification
    - Updated readme file with Swagger API Documentation and NSwag Client Generation secction
    
    * Updated client location in solution and save nswag file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# aspnetcore-api
-ASP.NET Core Web API Template
+# ASP.NET-Core Web API Template
+
+## Swagger API Documentation
+...
+
+## NSwag Client Generation
+...
+
+****
+### References
+- [ASP.NET Core web API documentation with Swagger / OpenAPI](https://docs.microsoft.com/en-us/aspnet/core/tutorials/web-api-help-pages-using-swagger)
---
+++ src/AspNetCore.API.Web/Startup.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace AspNetCore.API.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {
+                    Version = "v1",
+                    Title = "AspNetCore.API.Web",
+                    Description = string.Empty,
+                    Contact = new OpenApiContact {
+                        Name = "Cristian Mayo",
+                        Email = "cristian.c.mayo@outlook.com",
+                        Url = new Uri("https://cristianmayo.github.io"),
+                    }
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "AspNetCore.API.Web V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
