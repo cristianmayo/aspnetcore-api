9a302cb359e070e902577d3115e2f3b8cd054d6d
Cristian Mayo <cristian.c.mayo@outlook.com>
2021-02-05 00:32:08 +0800

    Added initial unit and integration test project

---
+++ tests/AspNetCore.API.Tests.Integration/IntegrationTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AspNetCore.API.Tests.Integration
+{
+    [TestClass]
+    public class IntegrationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
