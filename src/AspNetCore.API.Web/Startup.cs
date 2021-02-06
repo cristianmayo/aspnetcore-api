9d22c22ba1b4b563923dc97fa6e8a75b2ce3b483
Cristian Mayo <cristian.mayo@outlook.com>
2021-02-07 07:10:18 +0800

    Updated Startup file and NuGet package references with NSwag.AspNetCore instead of Swashbuckle.AspNetCore

--- src/AspNetCore.API.Web/Startup.cs
+++ src/AspNetCore.API.Web/Startup.cs
@@ -3,10 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using System;
-using System.IO;
-using System.Reflection;
 
 namespace AspNetCore.API.Web
 {
@@ -23,22 +19,9 @@ namespace AspNetCore.API.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo {
-                    Version = "v1",
-                    Title = "AspNetCore.API.Web",
-                    Description = string.Empty,
-                    Contact = new OpenApiContact {
-                        Name = "Cristian Mayo",
-                        Email = "cristian.c.mayo@outlook.com",
-                        Url = new Uri("https://cristianmayo.github.io"),
-                    }
-                });
-
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
+            services.AddSwaggerDocument(config => {
+                config.Title = "AspNetCore.API.Web";
+                config.Description = string.Empty;
             });
         }
 
@@ -50,11 +33,8 @@ namespace AspNetCore.API.Web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseSwagger();
-            app.UseSwaggerUI(c => {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "AspNetCore.API.Web V1");
-                c.RoutePrefix = string.Empty;
-            });
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
 
             app.UseHttpsRedirection();
 
